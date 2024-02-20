2b850958b7164cce044948c7cee4eeeef1df8a4e
danya-trikov <dtrikov.od@gmail.com>
2024-02-20 22:13:39 +0200

    added script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// let userCountry = prompt("Which country do you want to visit?")
+let  budjet= prompt("Enter your travel budjet");
+let userBudget = parseInt(budjet);
+
+if(userBudget === 500){
+    console.log("Ukraine is your option");
+}
+else {
+    console.log("Try later");
+}
